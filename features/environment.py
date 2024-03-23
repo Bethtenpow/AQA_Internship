299579fd3afd3d9e5b083c12cc3b39bf1e33c08a
Elizabeth.T <beth10pow@gmail.com>
2024-03-22 21:11:35 -0400

    Headless mode of chrome Task 3

--- features/environment.py
+++ features/environment.py
@@ -11,15 +11,29 @@ def browser_init(context):
     """
     :param context: Behave context
     """
+
+#Chrome
     # driver_path = ChromeDriverManager().install()
     # service = Service(driver_path)
     # context.driver = webdriver.Chrome(service=service)
 
-    driver_path = GeckoDriverManager().install()
-    service = Service(driver_path)
-    context.driver = webdriver.Firefox(service=service)
 
-    context.driver.maximize_window()
+#Firefox
+    # driver_path = GeckoDriverManager().install()
+    # service = Service(driver_path)
+    # context.driver = webdriver.Firefox(service=service)
+
+#Headless Chrome
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('--window-size=1920x1080')
+    service = Service(ChromeDriverManager().install())
+    context.driver = webdriver.Chrome(
+        options=options,
+        service=service
+    )
+
+    # context.driver.maximize_window()
 
     context.driver.implicitly_wait(4)
     context.wait = WebDriverWait(context.driver, 15)
