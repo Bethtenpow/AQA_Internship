98ae637eae98a10f1a6283e8da2ea0c19b9a863a
Elizabeth.T <beth10pow@gmail.com>
2024-03-27 22:01:41 -0400

    Cloud testing task

--- features/environment.py
+++ features/environment.py
@@ -5,6 +5,7 @@ from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from app.application import Application
+from selenium.webdriver.chrome.options import Options
 
 
 def browser_init(context):
@@ -24,21 +25,41 @@ def browser_init(context):
     # context.driver = webdriver.Firefox(service=service)
 
 #Headless Chrome
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    options.add_argument('--window-size=1920x1080')
-    service = Service(ChromeDriverManager().install())
-    context.driver = webdriver.Chrome(
-        options=options,
-        service=service
-    )
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('headless')
+    # options.add_argument('--window-size=1920x1080')
+    # service = Service(ChromeDriverManager().install())
+    # context.driver = webdriver.Chrome(
+    #     options=options,
+    #     service=service
+    # )
 
     # context.driver.maximize_window()
+
+    # context.driver.implicitly_wait(4)
+    # context.wait = WebDriverWait(context.driver, 15)
+    # context.app = Application(context.driver)
+
+    ##BROWSERSTACK##
+    bs_user = 'elizabethtenpow_F7SmpD'
+    bs_key = 'k7RLEpXzytZZCiPze3sV'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+
+    options = Options()
+    bstack_options = {
+        'os': 'Windows',
+        'osVersion': '10',
+        'browserName': 'Firefox',
+        'sessionName': 'User clicks on “Connect the company” button and can use the form to register a new agency'
+    }
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
+
+    context.driver.maximize_window()
 
     context.driver.implicitly_wait(4)
     context.wait = WebDriverWait(context.driver, 15)
     context.app = Application(context.driver)
-
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
